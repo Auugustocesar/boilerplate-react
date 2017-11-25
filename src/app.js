fd6cfffd2b80fe93fd0615322f48bcb5f4cda7f7
Augusto Cesar <augusto.gonzaga2010@gmail.com>
2017-11-25 10:32:04 -0200

    boilerplate

---
+++ src/app.js
@@ -0,0 +1,15 @@
+'use strict'
+
+import React from 'react'
+import createReactClass from 'create-react-class'
+
+const App = createReactClass({
+
+  render: function () {
+    return (
+      <div>Aplicação</div>
+    )
+  }
+})
+
+export default App
